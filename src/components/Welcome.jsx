5b150b5c0ca417053660d9786f593b1f6c8d464d
Stella Maia <stellamaia97@hotmail.com>
2023-11-27 00:43:07 -0300

    Configurações  Iniciais

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+const Welcome = () => {
+    return (
+        <div id="welcome">
+            <h2>Seja bem-vindo</h2>
+            <p>Clique no botão abaixo:</p>
+            <button>Iniciar</button>
+            <img src={Quiz} alt="Início do Quiz" />
+        </div>
+    )
+}
+export default Welcome
