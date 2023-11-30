bd461375c17c0f901d1cbbd279c168696010322b
stella <stellamaia97@hotmail.com>
2023-11-30 16:23:11 -0300

    Adicionar lógica para renderizar componente de boas-vindas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+
+import { useContext } from 'react'
+import { QuizContext } from './context/quiz'
 import Welcome from './components/Welcome'
+import  Question from './components/Question'
 import './App.css'
 
 function App() {
-
+const [quizState, dispatch] = useContext(QuizContext)
   return (
 
     <div className='App'> 
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === 'Start' && <Welcome />}
+      {quizState.gameStage === 'Playing' && <Question />}
     </div>
 
   )
 }
 
 export default App
+
