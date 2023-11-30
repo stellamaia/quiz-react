e6358b73dcc96074e41eaf6bb6fde9d78dff6b1c
Stella Maia <stellamaia97@hotmail.com>
2023-11-30 14:47:12 -0300

    Estrutura inicial do contexto e do provedor para o Quiz

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,9 +1,27 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+//useReducer gerencia estados mais complexos e também fazer alterçãoes com base no projeto
+import questions from '../data/questions';
+const STAGES = ['Start', 'Playing', 'End']
+//Cria o objeto initialState e cria propriedades que referem ao jogo
+const initialState = {
+    gameState: STAGES[0],//estagio inicial do jogo
+    questions//perguntas do jogo
+}
+
+const quizReducer = (state, action)=> {
+console.log(state, action);
+switch(action.type){
+    case 'CHANGE_STATE':
+        return state;
+        default:
+            return state;
+}
+};
 export const QuizContext = createContext()
-export const QuizProvider = ({children}) => {
-    return (
-        <QuizContext.Provider >
-            {children}
-        </QuizContext.Provider>
-    )
+
+export const QuizProvider = ({ children }) => {
+    const value = useReducer(quizReducer, initialState)
+
+    return <QuizContext.Provider value={value}> {children} </QuizContext.Provider>
+
 }
